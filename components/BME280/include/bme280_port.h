9db61ad0739ff6de25765a55ca0549db88049eb4
Víctor Tamayo Olea <vtamayo@uma.es>
2022-12-05 19:37:02 +0100

    Format

--- components/BME280/include/bme280_port.h
+++ components/BME280/include/bme280_port.h
@@ -15,12 +15,12 @@ extern "C" {
 
 #include <stdio.h>
 
+#include "bme280_defs.h"
+
 /***************************************************************************/
 
 /*!                 User function prototypes
  ****************************************************************************/
-
-
 
 /*!
  *  @brief Function for reading the sensor's registers through I2C bus.
@@ -36,15 +36,15 @@ extern "C" {
  *  @retval != BMP2_INTF_RET_SUCCESS -> Failure.
  *
  */
-int8_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void *intf_ptr);
-
-
+int8_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length,
+                     void *intf_ptr);
 
 /*!
  *  @brief Function for writing the sensor's registers through I2C bus.
  *
  *  @param[in] reg_addr : Register address.
- *  @param[in] reg_data : Pointer to the data buffer whose value is to be written.
+ *  @param[in] reg_data : Pointer to the data buffer whose value is to be
+ * written.
  *  @param[in] length   : No of bytes to write.
  *  @param[in] intf_ptr : Interface pointer
  *
@@ -54,11 +54,12 @@ int8_t user_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t length, void 
  *  @retval != BMP2_INTF_RET_SUCCESS -> Failure.
  *
  */
-int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *reg_data, uint32_t length, void *intf_ptr);
+int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *reg_data,
+                      uint32_t length, void *intf_ptr);
 
 /*!
- *  @brief This function provides the delay for required time (Microsecond) as per the input provided in some of the
- *  APIs.
+ *  @brief This function provides the delay for required time (Microsecond) as
+ * per the input provided in some of the APIs.
  *
  *  @param[in] period_us  : The required wait time in microsecond.
  *  @param[in] intf_ptr   : Interface pointer
@@ -77,8 +78,9 @@ void user_delay_us(uint32_t period_us, void *intf_ptr);
  *  @retval < 0 -> Failure
  */
 int8_t bme280_I2C_init(struct bme280_dev *dev);
-void print_sensor_data(struct bme280_data *comp_data);
-int8_t stream_sensor_data_forced_mode(struct bme280_dev *dev, struct bme280_data *comp_data);
+void   print_sensor_data(struct bme280_data *comp_data);
+int8_t stream_sensor_data_forced_mode(struct bme280_dev  *dev,
+                                      struct bme280_data *comp_data);
 
 #ifdef __cplusplus
 }
--- main/sensors.c
+++ main/sensors.c
@@ -1,59 +1,54 @@
 
+#include "sensors.h"
+
 #include <stdio.h>
 #include <string.h>
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-#include "esp_log.h"
-
-#include "sensors.h"
-#include "node_config.h"
 #include "bme280.h"
 #include "bme280_port.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "node_config.h"
 
-
-//defines
+// defines
 #define TSK_SIZE_SENSOR (4 * 1024)
 
 #define QUEUE_LENGTH_SENSOR 16
-#define BME280_MEASURES_NUM     3
+#define BME280_MEASURES_NUM 3
 
 // Prototypes
 void sensor_task(void *pvParameters);
 
-void sensors_init_task(measure_handler_t *measure_handler){
-
-    xTaskCreate(&sensor_task, "tsk_sensor", TSK_SIZE_SENSOR, measure_handler, 5, NULL);
+void sensors_init_task(measure_handler_t *measure_handler) {
+    xTaskCreate(&sensor_task, "tsk_sensor", TSK_SIZE_SENSOR, measure_handler, 5,
+                NULL);
 }
 
-
-void sensor_task(void *pvParameters)
-{
-    measure_handler_t measure_handler = *(measure_handler_t *) pvParameters;
-    struct bme280_dev dev;
+void sensor_task(void *pvParameters) {
+    measure_handler_t  measure_handler = *(measure_handler_t *)pvParameters;
+    struct bme280_dev  dev;
     struct bme280_data sensor_data;
-    measure_t bme280[BME280_MEASURES_NUM];
+    measure_t          bme280[BME280_MEASURES_NUM];
 
     bme280_I2C_init(&dev);
 
-    while (true)
-    {
+    while (true) {
         stream_sensor_data_forced_mode(&dev, &sensor_data);
-        print_sensor_data(&sensor_data);
         ESP_LOGI(pcTaskGetName(NULL), "Sensor data");
 
         bme280[0].value = sensor_data.humidity;
-        bme280[0].type = AIR_HUMIDITY;
+        bme280[0].type  = AIR_HUMIDITY;
 
         bme280[1].value = sensor_data.pressure;
-        bme280[1].type = AIR_PRESSURE;
+        bme280[1].type  = AIR_PRESSURE;
 
         bme280[2].value = sensor_data.temperature;
-        bme280[2].type = AIR_TEMPERATURE;
-        
-        for(int i = 0; i < BME280_MEASURES_NUM; i++){
-            ESP_LOGI(pcTaskGetName(NULL), "Push value %f type %d", bme280[i].value, bme280[i].type);
+        bme280[2].type  = AIR_TEMPERATURE;
+
+        for (int i = 0; i < BME280_MEASURES_NUM; i++) {
+            ESP_LOGI(pcTaskGetName(NULL), "Push value %f type %d",
+                     bme280[i].value, bme280[i].type);
             measurements_add(&measure_handler, &bme280[i]);
         }
         measurements_notify(&measure_handler);
